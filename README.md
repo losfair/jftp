26930b073193c544b700d9d0fc01b08d73413d0a
losfair <zhy20000919@hotmail.com>
2021-09-25 08:30:50 +0000

    Done

---
+++ README.md
@@ -0,0 +1,22 @@
+# jftp
+
+Just Forward The Port. A simple port forwarder.
+
+## Usage
+
+```
+jftp 0.1.0
+Just Forward The Port. A simple port forwarder.
+
+USAGE:
+    jftp [OPTIONS] --connect <connect> --listen <listen>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -c, --connect <connect>          The address to connect to
+    -l, --listen <listen>            The address to listen on
+    -t, --timeout-ms <timeout-ms>    Connect timeout in milliseconds [default: 30000]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,57 @@
+use std::time::Duration;
+
 use anyhow::Result;
+use structopt::StructOpt;
+use tokio::net::{TcpListener, TcpStream};
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+  name = "jftp",
+  about = "Just Forward The Port. A simple port forwarder."
+)]
+struct Opt {
+  /// The address to listen on.
+  #[structopt(short, long)]
+  listen: String,
+
+  /// The address to connect to.
+  #[structopt(short, long)]
+  connect: String,
+
+  /// Connect timeout in milliseconds.
+  #[structopt(short, long, default_value = "30000")]
+  timeout_ms: u64,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  println!("Hello, world!");
-  Ok(())
+  let opt = Opt::from_args();
+  pretty_env_logger::init();
+
+  let sock = TcpListener::bind(&opt.listen).await?;
+  log::info!("Listening on {}.", sock.local_addr()?);
+
+  loop {
+    let (mut incoming, peer) = sock.accept().await?;
+    log::info!("Accepted connection from {}.", peer);
+    let connect = opt.connect.clone();
+    let timeout = Duration::from_millis(opt.timeout_ms);
+    tokio::spawn(async move {
+      tokio::select! {
+        res = TcpStream::connect(&connect) => {
+          match res {
+            Ok(mut backend) => {
+              let _ = tokio::io::copy_bidirectional(&mut incoming, &mut backend).await;
+            }
+            Err(e) => {
+              log::error!("Unable to connect to backend {}: {:?}", connect, e);
+            }
+          }
+        }
+        _ = tokio::time::sleep(timeout) => {
+          log::error!("Timeout connecting to backend {} after {:?}.", connect, timeout);
+        }
+      }
+    });
+  }
 }
